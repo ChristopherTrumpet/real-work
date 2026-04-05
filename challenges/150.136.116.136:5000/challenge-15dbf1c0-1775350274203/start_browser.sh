f3d342c4c69ab76f21c9aa52bfa7255c61c4ee19
Christopher Trumpet <christopher.m.t2005@gmail.com>
2026-04-04 20:53:28 -0400

    fixed

--- challenges/150.136.116.136:5000/challenge-15dbf1c0-1775350274203/start_browser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wait for the web server to be ready (port 3000 for node, port 8080 for static/python simple server)
-TARGET_PORT=8080
-while ! curl -s http://localhost:$TARGET_PORT > /dev/null; do
-  sleep 2
-done
-firefox http://localhost:$TARGET_PORT &
